e71715e21cea3cf243978e2d344d64605485fdd3
Francisco J. Rodriguez Revidiego <forgex18@gmail.com>
2017-06-14 18:34:51 +0200

    Version final

--- instalacion_abassy.sh
+++ instalacion_abassy.sh
@@ -2,7 +2,6 @@ echo "Se va a proceder a entrar en MySQL server para crear la base de datos de A
 echo "Introduzca usuario de MySQL: "
 read  user
 mysql -u $user -p -e "create database abassy_db" 
-mysql -u $user -p abassy_db < abassy_db.sql 
 cd ./main/
 sudo mvn tomcat:run
 echo "Base de datos instalada"
